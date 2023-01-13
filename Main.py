4a54defa9385e8b93ab97d0c2a05623b53546d1e
Ville Nevasuo <ville1.nevasuo@gmail.com>
2023-01-13 19:33:01 +0200

    initialize framework for time series for item data

--- Main.py
+++ Main.py
@@ -9,73 +9,79 @@ ca = certifi.where()
 
 load_dotenv()
 
-client = MongoClient(
-    "mongodb+srv://wildu55:"+os.environ["dbpassword"]+"@tsmtest.f0cnqjq.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
-print(client.list_database_names())
-db = client.test
 
-ah = db.test1
+def insert_database(data):
 
-personDocument = {
-    "name": {"first": "Alan", "last": "Turing"},
-    "birth": datetime.datetime(1912, 6, 23),
-    "death": datetime.datetime(1954, 6, 7),
-    "contribs": ["Turing machine", "Turing test", "Turingery"],
-    "views": 1250000
-}
+    client = MongoClient(
+        "mongodb+srv://wildu55:"+os.environ["dbpassword"]+"@tsmtest.f0cnqjq.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
+    print(client.list_database_names())
+    db = client.ahdata
 
-result = ah.insert_one(personDocument)
+    ah = db.itemdata
+
+    ah.insert_one(data)
 
 
-# def authenticate():
+def authenticate():
 
-#     url = 'https://auth.tradeskillmaster.com/oauth2/token'
+    url = 'https://auth.tradeskillmaster.com/oauth2/token'
 
-#     myobj = {
-#         "client_id": "c260f00d-1071-409a-992f-dda2e5498536",
+    myobj = {
+        "client_id": "c260f00d-1071-409a-992f-dda2e5498536",
 
-#         "grant_type": "api_token",
+        "grant_type": "api_token",
 
-#         "scope": "app:realm-api app:pricing-api",
+        "scope": "app:realm-api app:pricing-api",
 
-#         "token": os.environ["token"]
-#     }
+        "token": os.environ["token"]
+    }
 
-#     x = requests.post(url, json=myobj)
-#     access_data = json.loads(x.text)
+    x = requests.post(url, json=myobj)
+    access_data = json.loads(x.text)
 
-#     return access_data["access_token"]
-
-
-# def query(url, access_token):
-
-#     x = requests.get(url, headers={
-#         'Authorization': f'Bearer {access_token}'})
-
-#     return json.loads(x.text)
+    return access_data["access_token"]
 
 
-# def main():
+def query(url, access_token):
 
-#     realm_url = 'https://realm-api.tradeskillmaster.com/realms-by-name/EU-Thekal'
-#     ah_url = 'https://realm-api.tradeskillmaster.com/auction-houses/468'
-#     item_url = 'https://pricing-api.tradeskillmaster.com/ah/468'
+    x = requests.get(url, headers={
+        'Authorization': f'Bearer {access_token}'})
 
-#     access_token = authenticate()
-
-#     realm_data = query(realm_url, access_token)
-#     ah_data = query(ah_url, access_token)
-#     item_data = query(item_url, access_token)
-
-#     thekal_id = realm_data["realmId"]
-#     region_id = ah_data["region"]["regionId"]
-
-#     for el in realm_data["auctionHouses"]:
-#         if el["type"] == "Horde":
-#             ah_id = el["auctionHouseId"]
-
-#     print(item_data[1])
-#     print(item_data[2])
+    return json.loads(x.text)
 
 
-# main()
+def main():
+
+    realm_url = 'https://realm-api.tradeskillmaster.com/realms-by-name/EU-Thekal'
+    ah_url = 'https://realm-api.tradeskillmaster.com/auction-houses/468'
+    item_url = 'https://pricing-api.tradeskillmaster.com/ah/468'
+
+    access_token = authenticate()
+
+    realm_data = query(realm_url, access_token)
+    ah_data = query(ah_url, access_token)
+    item_data = query(item_url, access_token)
+
+    print(type(item_data))
+    print(len(item_data))
+
+    thekal_id = realm_data["realmId"]
+    region_id = ah_data["region"]["regionId"]
+
+    print("thekal id", thekal_id)
+    print("region id", region_id)
+
+    for el in realm_data["auctionHouses"]:
+        if el["type"] == "Horde":
+            ah_id = el["auctionHouseId"]
+
+    # for line in item_data:
+    #     if line["minBuyout"] > 0:
+    #         print(line["itemId"])
+
+    print(item_data[129])
+
+#    insert_database(item_data)
+ 
+
+main()
